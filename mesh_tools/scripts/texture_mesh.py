0cabe384a068b8028a9f3bfdd15cf772809e532e
Ziteng Lu <108641742+lzt02@users.noreply.github.com>
2025-08-14 15:33:02 +0800

    fix: Update texture_mesh.py

--- mesh_tools/scripts/texture_mesh.py
+++ mesh_tools/scripts/texture_mesh.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
               required=True,
               help='Directory containing images')
 @click.option('--cameras_dir', '-s', 
-              type=click.Path(exists=True, file_okay=False, resolve_path=True),
+              type=click.Path(exists=True, file_okay=True, resolve_path=True),
               required=True,
               help='Directory containing cameras')
 @click.option('--model_path', '-s',
